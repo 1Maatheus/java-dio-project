5851c295a0368d46d431108bfec19ae58f6582d2
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-09-13 18:12:34 -0300

    Commit

--- java-system/src/candidatura/ProcessoSeletivo.java
+++ java-system/src/candidatura/ProcessoSeletivo.java
@@ -1,9 +1,39 @@
 package candidatura;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        imprimirSelecionados();
+        String [] candidatos = {"Matheus", "Nayara", "Otavio", "Marcia", "Tereza"};
+        for(String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando) {
+                tentativasRealizadas++;
+            }else {
+                System.out.println("Contato realizado com sucesso!");
+            }
+        }while(continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu) {
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + "º tentativa!");
+        }else {
+            System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentativas realizadas!");
+        }
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
     }
 
     static void imprimirSelecionados() {
